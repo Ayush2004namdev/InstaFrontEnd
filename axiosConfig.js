ab1672b113b000e9093a54ae6171c2ea2821ad97
AYUSH2004NAMDEV <ayush2004namdev@gmail.com>
2024-06-18 15:50:19 +0530

    new commit

--- axiosConfig.js
+++ axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000'; // Replace with your backend's URL
+axios.defaults.baseURL = 'https://insta-backend-woad.vercel.app/'; // Replace with your backend's URL
 
 export default axios;
 
